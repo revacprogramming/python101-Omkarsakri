09260f01037beca21b0e5baa8f7ee90b37fa43ff
Omkarsakri <96863979+Omkarsakri@users.noreply.github.com>
2022-06-30 17:15:46 +0000

    8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,4 +1,14 @@
-# Files 
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+total=0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count = count+1
+        x=line.lstrip("X-DSPAM-Confidence:")
+        x=float(x)
+        total=total+x
+print("Average spam confidence:",total/count)# Files 
 
 filename = "dataset/mbox-short.txt"
 doing
--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,27 @@
-# Lists
+fname = input("Enter file name: ")
+fh = open(fname)
+x= list()
+y=fh.read()
+words=y.split()
 
-filename = "dataset/romeo.txt"
+
+for nfname = input("Enter file name: ")
+fh = open(fname)
+x= list()
+y=fh.read()
+words=y.split()
+
+
+for n in words: 
+    if n not in x:
+        x.append(n)
+        
+x.sort()
+print(x)
+
+ in words: 
+    if n not in x:
+        x.append(n)
+        
+x.sort()
+print(x)
