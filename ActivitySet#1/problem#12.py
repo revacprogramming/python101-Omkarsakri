dad3994ed820b883bd682bf3f29bf1bc257101d5
Omkarsakri <96863979+Omkarsakri@users.noreply.github.com>
2022-07-01 00:43:40 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,21 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+import re
+name = input("Enter file: ")
+if len(name) < 1:
+    name = "mbox-short.txt"
+naam = open(name)
+g=list()
+omkar={}
+for line in naam:
+  m=re.search("From ")
+    m=line.split()
+    if len(m)>0
+      a=m[1]
+      g.append(a)
+for word in g:
+    omkar[word]=omkar.get(word,0)+1
+biggest=None
+for word,s in omkar.items():
+    if biggest is None or s>biggest:
+        biggest=s
+        bigword=word
+print(bigword,biggest)
