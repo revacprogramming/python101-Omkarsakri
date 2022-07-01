851e4c2086d309db939a871a119a25dcb7b1fa2b
Omkarsakri <96863979+Omkarsakri@users.noreply.github.com>
2022-07-01 08:57:29 +0530

    Update problem#09.py

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -5,21 +5,7 @@ y=fh.read()
 words=y.split()
 
 
-for nfname = input("Enter file name: ")
-fh = open(fname)
-x= list()
-y=fh.read()
-words=y.split()
-
-
 for n in words: 
-    if n not in x:
-        x.append(n)
-        
-x.sort()
-print(x)
-
- in words: 
     if n not in x:
         x.append(n)
         
