2bcf493221f2fcc41fb5791f9220fecfed6f6dec
Omkarsakri <96863979+Omkarsakri@users.noreply.github.com>
2022-07-07 21:25:45 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,21 +1,18 @@
-import re
-name = input("Enter file: ")
-if len(name) < 1:
-    name = "mbox-short.txt"
-naam = open(name)
-g=list()
-omkar={}
-for line in naam:
-  m=re.search("From ")
-    m=line.split()
-    if len(m)>0
-      a=m[1]
-      g.append(a)
-for word in g:
-    omkar[word]=omkar.get(word,0)+1
-biggest=None
-for word,s in omkar.items():
-    if biggest is None or s>biggest:
-        biggest=s
-        bigword=word
-print(bigword,biggest)
+"""10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below."""
+
+
+file = input("Enter the file name: ")
+fh = open(file,'r')
+details = {}
+for l in fh:
+    if l.startswith("From "):
+        time = l.split()[5]
+        hr = time.split(":")[0]
+        details[hr] = details.get(hr,0)+1
+lst = list(details.items())
+lst.sort()
+print(lst)
+"""for t in lst:
+    print(t[0], t[1])"""
