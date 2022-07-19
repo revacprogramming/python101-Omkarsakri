6c58a0ac8e7e88830ec4f39f8df9089d6026e81a
Omkarsakri <96863979+Omkarsakri@users.noreply.github.com>
2022-07-19 15:32:19 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,5 +1,7 @@
 # Tuples
-
+#10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+#From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+#Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
@@ -14,9 +16,9 @@ for line in handle:
     if line.startswith("From"):
         y=line.split()
         a=y[5]
-	maggy=a.split(':')
-    maharshi=maggy[0]
-    l.append(maharshi)
+	can=a.split(':')
+    candle=can[0]
+    l.append(candle)
     l.sort()
 for word in l:
     d[word]=d.get(word,0)+1
